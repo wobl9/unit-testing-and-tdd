a21dde3eb7438dc29cd340002661bbc1b9ad0cd0
rualdd3 <Dmitry.LEBED@raiffeisen.ru>
2020-10-27 14:03:16 +0500

    created client constructor test

--- src/main/java/com/acme/banking/dbo/domain/Client.java
+++ src/main/java/com/acme/banking/dbo/domain/Client.java
@@ -8,16 +8,15 @@ public class Client {
     private UUID id;
     private String name;
     private Collection<Account> accounts = new ArrayList<>(); //TODO
-
     public Client(UUID id, String name) {
+        if (id == null) throw new IllegalArgumentException("id is null");
+        if (name == null || name.isEmpty()) throw new IllegalArgumentException("Invalid name");
         this.id = id;
         this.name = name;
     }
-
     public UUID getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
---
+++ src/test/java/com/acme/banking/dbo/ClientTests.java
@@ -0,0 +1,21 @@
+package com.acme.banking.dbo;
+
+import com.acme.banking.dbo.domain.Client;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClientTests {
+
+    private static final UUID VALID_ID = UUID.randomUUID();
+    private static final String VALID_NAME = "TEST_NAME";
+
+    @Test
+    public void shouldCreateNewClientWhenValidArguments() {
+        Client sut = new Client(VALID_ID, VALID_NAME);
+        assertEquals(sut.getId(), VALID_ID);
+        assertEquals(sut.getName(), VALID_NAME);
+    }
+}
